1acb6169b04c1aef4dc2adb4003dcb2517397e5e
Rifat Arefin <arefinrifat786@gmail.com>
2024-05-09 17:10:04 +0600

    Version 4

--- curriculum/course_list.php
+++ curriculum/course_list.php
@@ -24,14 +24,43 @@ $PAGE->requires->css('/local/curriculum/css/style.css');
 global $DB, $OUTPUT;
 //$catId = optional_param('catId', 0, PARAM_INT);*************
 
+/************************/
+// INSERT INTO mdl_track_courses (user_id, cat_id, course_id, complete)
+// SELECT 
+//     3 AS user_id,
+//     5 AS cat_id,
+//     id,
+//     0 AS complete
+// FROM 
+//     mdl_course
+// WHERE 
+//     category = 5;
+/************************/
+
 // find all the categories except the top category
 //$sql = "SELECT * FROM {course} WHERE id = $catId";***********
 $sql = "SELECT * FROM {course} WHERE category = 5";//**************** */
 $categories = array_values($DB->get_records_sql($sql));
 $mytext= get_string('av_crs', 'local_curriculum');
 
+$sql = "SELECT course_id, complete FROM {track_courses} WHERE user_id =3 ";
+$temp = array_values($DB->get_records_sql($sql));
+
+$role_id = 5;
+
 // ***** page body starts here *****
 echo $OUTPUT->header();
+
+foreach($temp as $tmp){
+    if(!$tmp->complete){
+        $instance = $DB->get_record('enrol', ['courseid' => $tmp->course_id, 'enrol' => 'manual']);
+        $enrolplugin = enrol_get_plugin($instance->enrol);
+        $enrolplugin->enrol_user($instance, 3,$role_id, time(),0);
+        break;
+    }
+}
+
+
 if(sizeof($categories)){
     $mytext = get_string('all_av_crs', 'local_curriculum');
 } 
